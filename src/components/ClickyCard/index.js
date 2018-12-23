251c69f16bbde3edd7bdfb235cda28d8920e317e
Tricia Norris <tricia@Tricias-MacBook-Air.local>
2018-12-23 11:30:12 -0500

    jumbotron css

--- src/components/ClickyCard/index.js
+++ src/components/ClickyCard/index.js
@@ -18,14 +18,4 @@ const ClickyCard = props => (
 );
 
 
-// function ClickyCard(props) {
-//   return (
-//     <div className="card">
-//       <div className="img-container">
-//         <img src={props.image} alt="Click Me!" />
-//       </div>
-//     </div>
-//   );
-// }
-
 export default ClickyCard;
--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,13 +1,11 @@
 import React from "react";
-// import ""
+import "../Jumbotron/style.css"
 
 function Jumbotron() {
   return (
     <div className="jumbotron">
       <h1>Richmond Rocks! Clicky Game</h1>
-        <h4>Click on an image of Richmond, VA to earn points, but don't click on any more than once!
-          Your memory is your best friend.
-        </h4>
+      <h4>Click on an image of Richmond, VA to earn points, but don't click on any more than once! Scroll down and start clicking!</h4>
     </div>
   );
 }
